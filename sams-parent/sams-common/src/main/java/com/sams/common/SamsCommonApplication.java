a6a813f5cba032d5d36c007d1a6a9699f333f7b9
gxzhang <gxzhang@kingbase.com.cn>
2019-02-13 12:41:28 +0800

    工程搭建初始化

---
+++ sams-parent/sams-common/src/main/java/com/sams/common/SamsCommonApplication.java
@@ -0,0 +1,14 @@
+package com.sams.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamsCommonApplication.class, args);
+    }
+
+}
+
---
+++ sams-parent/sams-entity/src/main/java/com/sams/entity/SamsEntityApplication.java
@@ -0,0 +1,14 @@
+package com.sams.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamsEntityApplication.class, args);
+    }
+
+}
+
